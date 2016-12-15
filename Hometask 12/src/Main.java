020ef1605c2580e7f8fffe940f15b9285383b992
Ingvarrr <falcon.only@gmail.com>
2016-12-15 15:04:37 +0200

    initial commit

--- Hometask 12/src/Main.java
+++ Hometask 12/src/Main.java
@@ -12,19 +12,19 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("/home/vig/IdeaProjects/Source_IT-L12/Hometask 12/src/SomeFile.txt");
+        File file = new File("Hometask 12/src/SomeFile.txt");
         if(file.createNewFile()){
             System.out.println("Файл создан");
         }else {
             System.out.println("Файл уже существует");
         }
-        Path filePath = Paths.get("/home/vig/IdeaProjects/Source_IT-L12/Hometask 12/src/SomeFile.txt");
+        Path filePath = Paths.get("Hometask 12/src/SomeFile.txt");
 
         byte[] fileInBytes = Files.readAllBytes(filePath);
         List<String> fileInStrings = Files.readAllLines(filePath);
 
-        File file1 = new File("/home/vig/IdeaProjects/Source_IT-L12/Hometask 12/src/AnotherFile.txt");
-        Path filePath2 = Paths.get("/home/vig/IdeaProjects/Source_IT-L12/Hometask 12/src/AnotherFile.txt");
+        File file1 = new File("Hometask 12/src/AnotherFile.txt");
+        Path filePath2 = Paths.get("Hometask 12/src/AnotherFile.txt");
 
         byte[] primitiveArray = {4,10,15,20,25};
         Files.write(filePath2, primitiveArray);
